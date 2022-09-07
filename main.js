278a16c8b753ae307473bd89fd303b5ccd4a0105
Josué González <60802835+15osvaldo@users.noreply.github.com>
2022-09-07 13:55:55 -0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function CSS(){
-    elemento = document.getElementById("img");
-    elemento.src = "/MySQL.jpg";    
-}
-
-function MYSQL(){
-    elemento = document.getElementById("img");
-    elemento.src = "/MySQL.jpg";    
-}
