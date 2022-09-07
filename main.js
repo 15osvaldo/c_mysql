49dec75719684586ec32bdcbeac9f3eee0c8df9a
Josué González <60802835+15osvaldo@users.noreply.github.com>
2022-09-07 13:45:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function CSS(){
+    elemento = document.getElementById("img");
+    elemento.src = "/MySQL.jpg";    
+}
+
+function MYSQL(){
+    elemento = document.getElementById("img");
+    elemento.src = "/MySQL.jpg";    
+}
